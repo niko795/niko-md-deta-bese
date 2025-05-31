3b0c3e090e6a488d49597de01fc2ec0b30dd4b11
niko795 <116344885+niko795@users.noreply.github.com>
2025-05-31 16:21:53 +0530

    Update deta.js

--- deta.js
+++ deta.js
@@ -2,7 +2,7 @@
     "button":"Follow US",
     "footer":"> *𝗥𝗲𝗽𝗼:* https://github\n\n ㋛> ",
     "buttonurl":"https://whatsapp.com/channel/0029Vb5vNWiDZ4Lcj9bgqB2l",
-    "imageurl":"https://files.catbox.moe/kdo4wa.jpg",
+    "imageurl":"https://files.catbox.moe/cs2b1p.jpg",
     "header":"> ☘ 𝗡𝗜𝗞𝗢 𝗠𝗗 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 ☂",
     "videourl":"https://raw.githubusercontent.com/athulakumara604/ASITHA-MD-DATABASE/main/Alivevoice/VID-20241113-WA0655.mp4",
     "channel" : "https://whatsapp.com/channel/0029Vb5vNWiDZ4Lcj9bgqB2l",
